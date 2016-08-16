40bc730d058590aa06801be87f5cee923f846221
diego1soler <dsoler1998@gmail.com>
2016-08-15 22:08:28 -0600

    La Factoría

--- src/FactoriaCalc.java
+++ src/FactoriaCalc.java
@@ -2,24 +2,39 @@
 
 public class FactoriaCalc {
 
-public static <E> StackArrayList<E> getPila(){
-	return new StackArrayList<E>();
-	}
-
-public static <E> StackVector<E>	getVector(){
+public static <E> AbstractStack getTipo(int eleccion1,int eleccion2){
+	
+	//Aqui ocurre la magia
+	switch(eleccion1){
+	
+	case 1:
 	return new StackVector<E>();
-}
-
-public static <E> SinglyLinkedList<E>	getSinlgyLinkedList(){
-	return new SinglyLinkedList<E>();
-}
-
-public static <E> DoubleLinkedList<E>	getDoubleLinkedList(){
-	return new DoubleLinkedList<E>();
-}
-
-public static <E> CircularLinkedList<E>	getCircularLinkedList(){
-	return new CircularLinkedList<E>();
+	
+	
+	case 2:
+	return new StackArrayList<E>();
+	
+	
+	case 3:
+	
+		if (eleccion2 ==1){
+			return new SinglyLinkedList<E>();
+		}
+		if (eleccion2 ==2){
+			return new DoubleLinkedList<E>();
+		}
+		
+		else{
+			return new CircularLinkedList<E>();
+		}
+		
+		
+		
+	}
+	return null;
+	
+	
+	
 }
 
 	
